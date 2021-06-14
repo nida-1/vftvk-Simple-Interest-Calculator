3e83defa58667072d581b3d908c5f280715d9fc1
nida-1 <nidairam16@gmail.com>
2021-06-14 22:27:18 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
+var slider = document.getElementById("rate");
+var output = document.getElementById("rvalue");
+output.innerHTML = slider.rvalue;
+
+function updateRate() 
+{
+    var rvalue = document.getElementById("rate").value;
+    document.getElementById("rvalue").innerText=rvalue;
+}
+
+slider.oninput = function(){
+ output.innerHTML = this.rvalue;
+    }
+
 function compute()
 {
     p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    
 
     var year = new Date().getFullYear()+parseInt(years);
     var interest = principal * years * rate /100;
-    result = document.getElementById("result");
+    var result = document.getElementById("result").interest;
+    if (principal==0 || principal<0)
+    {
+        alert("enter a positive number");
+    }
+    else
+    {
     document.getElementById("result").innerHTML="If you deposit "+principal+
     "\<br\>at an interest rate of "+rate+
-    "%\<br\>You will receive an amount of "+result+"\<br\>in the year "+year+
+    "%\<br\>You will receive an amount of "+interest+"\<br\>in the year "+year+
     "\<br\>";
-
-}
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-    
-    
-}
-function slider(){
-rateval.onchange = rateval.innerHTML = this.value
     }
-     
+}  
+
+
