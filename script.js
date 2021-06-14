bfba539bbde2164ffa6a7056f11618c71aa3ef21
nida-1 <nidairam16@gmail.com>
2021-06-14 21:34:10 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    
+
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = principal * years * rate /100;
+    result = document.getElementById("result");
+    document.getElementById("result").innerHTML="If you deposit "+principal+
+    "\<br\>at an interest rate of "+rate+
+    "%\<br\>You will receive an amount of "+result+"\<br\>in the year "+year+
+    "\<br\>";
+
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+    
     
 }
-        
+function slider(){
+rateval.onchange = rateval.innerHTML = this.value
+    }
+     
